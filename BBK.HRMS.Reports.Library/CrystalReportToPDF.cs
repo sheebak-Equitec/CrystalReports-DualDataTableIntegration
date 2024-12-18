180ccc2efbad7082a2cc5add1f2942db190cc2fe
sheeba-khan <124671004+sheebak-Equitec@users.noreply.github.com>
2024-12-18 12:42:04 +0530

    Initial commit

---
+++ BBK.HRMS.Reports.Library/CrystalReportToPDF.cs
@@ -0,0 +1,90 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Shared;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace BBK.HRMS.Reports
+{
+    public class CrystalReportToPDF
+    {
+        private readonly DataTable inputParametersTable;
+        private readonly string connectionString;
+        private readonly string reportPath;
+
+        public CrystalReportToPDF(string reportPath, DataTable inputParameters, string connectionString)
+        {
+            this.connectionString = connectionString;
+            this.reportPath = reportPath;
+            this.inputParametersTable = inputParameters;
+        }
+
+        public void CreateReportPDF()
+        {
+            ReportDocument reportDocument = new ReportDocument();
+
+            try
+            {
+                reportDocument.Load(reportPath);
+                reportDocument.Database.Tables[0].ApplyLogOnInfo(LogonInfo());
+
+                DataTable reportDataSourceTable = GetReportDataTable(inputParametersTable, "PROC5_RPT_SSS_LIST_OF_MONTHLY_DISTRIBUTION");
+                DataSet ds = new DataSet();
+                ds.Tables.Add(reportDataSourceTable);
+                ds.Tables.Add(inputParametersTable);
+                reportDocument.SetDataSource(ds);
+
+                using (Stream stream = reportDocument.ExportToStream(ExportFormatType.PortableDocFormat))
+                {
+                    byte[] bytes = new MemoryStream().ToArray();
+                    stream.CopyTo(new MemoryStream(bytes));
+                    string base64EncodedString = Convert.ToBase64String(bytes);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+            reportDocument.Dispose();
+        }
+
+        private TableLogOnInfo LogonInfo()
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder(connectionString);
+            return new TableLogOnInfo
+            {
+                ConnectionInfo =
+                {
+                    ServerName = builder.DataSource,
+                    DatabaseName = builder.InitialCatalog,
+                    UserID = builder.UserID,
+                    Password = builder.Password
+                }
+            };
+        }
+
+        private DataTable GetReportDataTable(DataTable inputParameters, string procedureName)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(procedureName, connection)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+
+                foreach (DataRow row in inputParameters.Rows)
+                {
+                    command.Parameters.AddWithValue(row["ParameterName"].ToString(), row["ParameterValue"]);
+                }
+
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                DataTable outputDataTable = new DataTable();
+                adapter.Fill(outputDataTable);
+
+                return outputDataTable;
+            }
+        }
+    }
+}
---
+++ BBK.HRMS.Reports/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Data;
+
+namespace BBK.HRMS.Reports
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string reportPath = "C:\\Users\\Equilap41\\Documents\\GitHub\\HRMS\\BBK-HRMS-MVC\\HRMS\\Reports\\M5\\rptM5_ListMnthDistribution.rpt";
+            string connectionString = "server=.;Database=BBKDB;User Id=sa;Password=123;TrustServerCertificate=True";
+            
+            DataTable inputParameters = GetInputParameters();
+            try
+            {
+
+                CrystalReportToPDF reportToPDF = new CrystalReportToPDF(reportPath, inputParameters, connectionString);
+                reportToPDF.CreateReportPDF();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred while generating the report:");
+                Console.WriteLine("Message: " + ex.Message);
+                Console.WriteLine("Stack Trace: " + ex.StackTrace);
+                Console.ReadLine();
+            }
+        }
+
+      
+        private static DataTable GetInputParameters()
+        {
+            DataTable inputParameters = new DataTable();
+            inputParameters.Columns.Add("ParameterName", typeof(string));
+            inputParameters.Columns.Add("ParameterValue", typeof(object));
+
+            inputParameters.Rows.Add("@i_Company_Cd", "BBK");
+            inputParameters.Rows.Add("@i_Group_Cd", "");
+            inputParameters.Rows.Add("@i_Division_Cd", "");
+            inputParameters.Rows.Add("@i_Department_Cd", "");
+            inputParameters.Rows.Add("@i_Section_Cd", "");
+            inputParameters.Rows.Add("@i_Emp_No", "1763");
+            inputParameters.Rows.Add("@i_For_Month", "01/01/2021");
+            inputParameters.Rows.Add("@i_Incharger_Emp_No", "1763");
+            inputParameters.Rows.Add("@i_Status", "A");
+            return inputParameters;
+        }
+    }
+}
